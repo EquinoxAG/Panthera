ffccd9e68788d0d17097a2292b994c6fbd52aa54
Alexander <equinox.malexander@outlook.de>
2015-08-23 00:59:30 +0200

    Saved the parameters of the intitialisation function locally to ensure that multiple virtual memory spaces could be processed

--- kernel/memory/vmemory.asm
+++ kernel/memory/vmemory.asm
@@ -1,1 +1,10 @@
 %include "memory/virtual_memory.inc"
+
+DeclareFunction InitialiseVirtualMemoryManager( bootup_pml4 )
+	ReserveStackSpace OperatedPML4, qword
+	UpdateStackPtr
+
+	mov_ts qword[ OperatedPML4 ], Arg_bootup_pml4
+
+
+EndFunction
